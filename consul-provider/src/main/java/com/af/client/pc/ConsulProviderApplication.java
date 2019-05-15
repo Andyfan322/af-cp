f7880df9c80ce8635d2347a6481c393532160ba7
andyFan <705048972@qq.com>
2019-05-15 17:13:30 +0800

    .

--- consul-provider/src/main/java/com/af/client/pc/ConsulProviderApplication.java
+++ consul-provider/src/main/java/com/af/client/pc/ConsulProviderApplication.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**provider
  * Consul 服务提供者
  *
  * @author andyFan
